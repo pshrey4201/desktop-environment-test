2ba8ae661c54292e00e0be686fc250944873d4b9
Shrey Patel <patelshrey4201@gmail.com>
2022-02-15 16:05:10 +0000

    Added code to allow the server to render a black screen. Added credits to README.md file.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A wlroots based desktop environment.
 
 Work in progress
+
+# Credits
+1. [wlroots](https://gitlab.freedesktop.org/wlroots/wlroots)
+2. [tinywl](https://gitlab.freedesktop.org/wlroots/wlroots/-/tree/master/tinywl)
--- main.c
+++ main.c
@@ -16,7 +16,52 @@ struct test_server {
 	struct wlr_scene *scene;
 
 	struct wlr_output_layout *output_layout;
+	struct wl_list outputs;
+	struct wl_listener new_output;
 };
+
+struct test_output {
+	struct wl_list link;
+	struct test_server *server;
+	struct wlr_output *wlr_output;
+	struct wl_listener frame;	
+};
+
+static void output_frame(struct wl_listener *listener, void *data) {
+	struct test_output *output = wl_container_of(listener, output, frame);
+	struct wlr_scene *scene = output->server->scene;
+
+	struct wlr_scene_output *scene_output = wlr_scene_get_scene_output(scene, output->wlr_output);
+
+	wlr_scene_output_commit(scene_output);
+
+	struct timespec now;
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	wlr_scene_output_send_frame_done(scene_output, &now);
+}
+
+static void server_new_output(struct wl_listener *listener, void *data) {
+	struct test_server *server = wl_container_of(listener, server, new_output);
+	struct wlr_output *wlr_output = data;
+
+	wlr_output_init_render(wlr_output,  server->allocator, server->renderer);
+
+	if(!wl_list_empty(&wlr_output->modes)) {
+		struct wlr_output_mode *mode = wlr_output_preferred_mode(wlr_output);
+		wlr_output_set_mode(wlr_output, mode);
+		wlr_output_enable(wlr_output, true);
+	}
+
+	struct test_output *output = calloc(1, sizeof(struct test_output));
+	output->wlr_output = wlr_output;
+	output->server = server;
+
+	output->frame.notify = output_frame;
+	wl_signal_add(&wlr_output->events.frame, &output->frame);
+	wl_list_insert(&server->outputs, &output->link);
+	wlr_output_layout_add_auto(server->output_layout, wlr_output);
+	
+}
 
 int main(int argc, char *argv[]) {
 
@@ -34,6 +79,10 @@ int main(int argc, char *argv[]) {
 	wlr_compositor_create(server.wl_display, server.renderer);
 
 	server.output_layout = wlr_output_layout_create();
+
+	wl_list_init(&server.outputs);
+	server.new_output.notify = server_new_output;
+	wl_signal_add(&server.backend->events.new_output, &server.new_output);
 
 	server.scene = wlr_scene_create();
 	wlr_scene_attach_output_layout(server.scene, server.output_layout);
